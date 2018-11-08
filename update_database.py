fcc22237b0110e008ce731dd69798863945f25d1
abargerbos <arnobargerbos@hotmail.com>
2018-11-08 15:34:32 +0100

    added html

--- update_database.py
+++ update_database.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-def create_dataframe(dict_list): #this should just be the output of the parser, lets remove it later
+def create_dataframe(dict_list): #this takes the output of the parser
+    df = pd.DataFrame(dict_list)
+    cols = ['title', 'author_list', 'published', 'arxiv_primary_category', 'id', 'link']
+    df = df[cols] #reorder columns
+    df.sort_values(by=['published'], ascending=False) #sorts by date
     return pd.DataFrame(dict_list)
 
 def update_database(df1, df2):
     df3 = pd.concat([df1,df2])
-    df3.drop_duplicates(subset=['id'], inplace=True, keep='last')
+    df3.drop_duplicates(subset=['id'], inplace=True, keep='last') #horribly inefficient
     return(df3)
 
-def create_html(df): #going to add some arguments to get decent output
+def create_html(df): #add some arguments to get decent output
     return df.to_html('filename.html')
