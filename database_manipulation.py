0a45bfec16de115903f23e604aec305f6599f975
abargerbos <arnobargerbos@hotmail.com>
2018-11-08 16:48:38 +0100

    Added some functionalities..

--- database_manipulation.py
+++ database_manipulation.py
@@ -12,11 +12,18 @@ def update_database(df1, df2):
     df3.sort_values(by=['published'], ascending=False) #sorts by date
     return(df3)
 
-def create_html(df): #add some arguments to get decent output, but shouldn't really be in here
+def create_html(df, filename): #add some arguments to get decent output, but shouldn't really be in here
     pd.set_option('display.max_rows', len(df))
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_colwidth', -1)
-    df.to_html('filename.html', index=False)
+    df.to_html(filename, index=False, justify='left')
     pd.reset_option('display.max_rows')
     pd.reset_option('display.max_columns')
     pd.reset_option('display.max_colwidth')
+
+def save_database(df, filename):    
+    df.to_pickle(filename)
+    
+def load_database(filename):
+    df = pd.read_pickle(filename)
+    return(df)
