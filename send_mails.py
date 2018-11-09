3d38079b50b77c93f74dcb1a7d25d75336f7f28b
nvanloo <n.nickvanloo@gmail.com>
2018-11-09 15:15:32 +0100

    Now with table

--- send_mails.py
+++ send_mails.py
@@ -1,28 +1,52 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import database_manipulation as dm
+import pandas as pd
+import time
+import datetime
+
+def _convert_time(val):
+    date = datetime.datetime.strptime(val,"%Y-%m-%d %H:%M:%S")
+    return date
+ 
+
+df3 = pd.read_pickle('dummydatabase.pkl')
+
+df3['published'] = df3['published'].apply(_convert_time)
+    
+now = datetime.datetime.now()
+delta = datetime.timedelta(days=7)
+now - delta
+
+df4 = df3.loc[df3['published'] > (now - delta)]
+
+dm.create_html(df4, 'Last_week_database.html')
 
 with open('Adress_list.txt', 'r') as f:
     toAddress = [line.strip() for line in f]
 fromaddr = "Majorana.Arxiv@gmail.com"
 
+Last_week_submissions = open('Last_week_database.html')
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = 'Topo'
 
-msg['Subject'] = "Python testing email"
+msg['Subject'] = "Arxiv Majorana submissions last week"
 
 body = '''Dear topo,
-This is a test mail using MIME. Seems to work well.
-This is the body of the mail.
+
+Here is a list of Majorana papers submitted last week. Enjoy reading :)
 
 Regards,
 Majorana.Arxiv
 
-'''
+''' 
 
 msg.attach(MIMEText(body, 'plain'))
+msg.attach(MIMEText(Last_week_submissions.read(),'html'))
 Message = msg.as_string()
+
 
 try:
     conn = smtplib.SMTP('smtp.gmail.com', 587) #smtp address and port
