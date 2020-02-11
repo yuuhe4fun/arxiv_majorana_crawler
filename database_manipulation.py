0cd44222632fed75a341cbb6750120e518ad0d92
Guanzhong Wang <cnhajzwgz@gmail.com>
2020-02-12 00:32:13 +0100

    made table look way better with css

--- database_manipulation.py
+++ database_manipulation.py
@@ -2,23 +2,23 @@ import pandas as pd
 import bs4
 
 def update_database(df1, df2):
-    df3 = pd.concat([df1,df2], sort=True)
-    df3.drop_duplicates(subset=['id'], inplace=True, keep='last') #could be done more efficiently?
+    df3 = pd.concat([df1, df2], sort=True)
+    df3.drop_duplicates(subset=['id'], inplace=True, keep='last') # could be done more efficiently?
     cols = ['title', 'author_list', 'published', 'arxiv_primary_category', 'id', 'link']
-    df3 = df3[cols] #reorder columns
+    df3 = df3[cols] # reorder columns
     return(df3.sort_values(by=['published'], ascending=False))
 
-def create_html(df, filename): #add some arguments to get decent output, but shouldn't really be in here
+def create_html(df, filename): # add some arguments to get decent output, but shouldn't really be in here
     pd.set_option('display.max_rows', len(df))
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_colwidth', -1)
     df['link'] = df['link'].apply(_make_clickable) #make hyperlinks clickable
-    #df['author_list'] = df['author_list'].apply(_make_utf8) #set correct encoding for html
+    # df['author_list'] = df['author_list'].apply(_make_utf8) #set correct encoding for html
     df.to_html(filename, escape=False, index=False, justify='left')
     pd.reset_option('display.max_rows')
     pd.reset_option('display.max_columns')
     pd.reset_option('display.max_colwidth')
-    
+
     with open(filename) as inf:
         txt = inf.read()
     soup = bs4.BeautifulSoup(txt, 'html5lib')
@@ -27,13 +27,17 @@ def create_html(df, filename): #add some arguments to get decent output, but sho
     metatag.attrs['content'] = 'text/html; charset=utf-8'
     soup.head.append(metatag)
     mathjaxtag2 = soup.new_tag('script')
-    mathjaxtag2.attrs['type'] = 'text/javascript' 
+    mathjaxtag2.attrs['type'] = 'text/javascript'
     mathjaxtag2.attrs['src'] = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML' 
     soup.head.insert(1, mathjaxtag2)
     mathjaxtag1 = soup.new_tag('script')
-    mathjaxtag1.attrs['type'] = 'text/x-mathjax-config' 
+    mathjaxtag1.attrs['type'] = 'text/x-mathjax-config'
     mathjaxtag1.append(r"MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});")
     soup.head.insert(1, mathjaxtag1)
+    csstag = soup.new_tag('style')
+    with open('table_style.css', 'r') as css_file:
+        csstag.string = css_file.read()
+    soup.head.append(csstag)
     with open(filename, "w") as outf:
         outf.write(str(soup))
 
