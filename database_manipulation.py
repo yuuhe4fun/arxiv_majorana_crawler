3e034b059c0533fe79cac7c98edf227170664afd
Guanzhong Wang <cnhajzwgz@gmail.com>
2020-07-31 22:48:43 +0200

    forgot what changed in dbmanip

--- database_manipulation.py
+++ database_manipulation.py
@@ -2,18 +2,20 @@ import pandas as pd
 import bs4
 import codecs
 
+
 def update_database(df1, df2):
     df3 = pd.concat([df1, df2], sort=True)
-    df3.drop_duplicates(subset=['id'], inplace=True, keep='last') # could be done more efficiently?
+    df3.drop_duplicates(subset=['id'], inplace=True, keep='last')  # could be done more efficiently?
     cols = ['title', 'author_list', 'published', 'arxiv_primary_category', 'id', 'link']
-    df3 = df3[cols] # reorder columns
-    return(df3.sort_values(by=['published'], ascending=False))
+    df3 = df3[cols]  # reorder columns
+    return df3.sort_values(by=['published'], ascending=False)
 
-def create_html(df, filename): # add some arguments to get decent output, but shouldn't really be in here
+
+def create_html(df, filename):  # add some arguments to get decent output, but shouldn't really be in here
     pd.set_option('display.max_rows', len(df))
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_colwidth', None)
-    df['link'] = df['link'].apply(_make_clickable) #make hyperlinks clickable
+    df['link'] = df['link'].apply(_make_clickable)  # make hyperlinks clickable
     # df['author_list'] = df['author_list'].apply(_make_utf8) #set correct encoding for html
     df.to_html(filename, escape=False, index=False, justify='left')
     pd.reset_option('display.max_rows')
@@ -29,7 +31,8 @@ def create_html(df, filename): # add some arguments to get decent output, but sh
     soup.head.append(metatag)
     mathjaxtag2 = soup.new_tag('script')
     mathjaxtag2.attrs['type'] = 'text/javascript'
-    mathjaxtag2.attrs['src'] = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML' 
+    mathjaxtag2.attrs[
+        'src'] = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
     soup.head.insert(1, mathjaxtag2)
     mathjaxtag1 = soup.new_tag('script')
     mathjaxtag1.attrs['type'] = 'text/x-mathjax-config'
@@ -41,6 +44,7 @@ def create_html(df, filename): # add some arguments to get decent output, but sh
     soup.head.append(csstag)
     with codecs.open(filename, "w", encoding='utf8') as outf:
         outf.write(str(soup))
+
 
 def _make_clickable(val):
     # target _blank to open new window
