564f99f25275f112b32e4ea7a006f522bc7a9455
Gaberber <cnhajzwgz@gmail.com>
2021-03-04 13:56:43 +0100

    updated installation path

--- fetch_arxiv.bat
+++ fetch_arxiv.bat
@@ -1,4 +1,4 @@
 @echo on
-call C:\Users\guanzhongwang\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
+call E:\Programs\Anaconda3\Scripts\activate.bat
 CD /D E:\Code\arxiv_majorana_crawler
-C:\Users\guanzhongwang\AppData\Local\Continuum\anaconda3\python.exe "fetch_arxiv.py"
+E:\Programs\Anaconda3\python.exe "fetch_arxiv.py"
